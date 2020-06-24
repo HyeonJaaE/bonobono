bb4231eb0b38d5a9839419235f5b4f64b5e17d8b
LeeHyeonJae <presentlee914@gmail.com>
2020-06-24 15:31:01 +0900

    directory setting

---
+++ src/Components/Home.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+const Home = () => {
+  let name: string;
+  name = "lee";
+  return(
+    <div>
+      <div>
+        <p>
+          Hello {name}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
