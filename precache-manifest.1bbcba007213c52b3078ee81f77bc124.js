ba9c6827b1f11de334ceb0cb17a1951ec19b6398
LeeHyeonJae <presentlee914@gmail.com>
2020-06-24 15:50:13 +0900

    Updates

---
+++ precache-manifest.1bbcba007213c52b3078ee81f77bc124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e64b6927f6a13c439b1b42d12bf4fb2a",
+    "url": "/bonobono/index.html"
+  },
+  {
+    "revision": "2ef656aaa0e1d48e55d1",
+    "url": "/bonobono/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8eef519f6e9d67245d24",
+    "url": "/bonobono/static/js/2.bd424ff0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bonobono/static/js/2.bd424ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ef656aaa0e1d48e55d1",
+    "url": "/bonobono/static/js/main.617b571d.chunk.js"
+  },
+  {
+    "revision": "f45b69df16c840da6a2e",
+    "url": "/bonobono/static/js/runtime-main.466fa1fb.js"
+  }
+]);
